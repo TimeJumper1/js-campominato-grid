2503654051e2a1cb8eacb878030ea40c613f82cc
TimeJumper1 <benassai.stefano.1@gmail.com>
2021-11-24 10:31:52 +0100

    completato

--- js/script.js
+++ js/script.js
@@ -1,21 +1,34 @@
 // popolare la griglia di tot quadrati in base alla difficolta
 // facile 100 , medio 81 , difficile 49
 // quando si clicca un quadrato si aggiunge la classe active
+let test = 0;
 
-let test = 100;
+
 document.getElementById('play').addEventListener('click', startGame);
 
-function startGame(){
+function startGame(difficolta){
     const gridContainer = document.getElementById('ms_grid');
     gridContainer.innerHTML = '';
-    for (let i = 1; i < 101; i++){
+    let difficoltas = document.getElementById('difficulty-selector').value;
+
+
+
+    if (difficoltas === '100'){
+        test = 100
+    }else if(difficoltas === '81'){
+        test = 81
+    }else if(difficoltas === '49'){
+        test = 49
+    } 
+    for (let i = 1; i < test+1; i++){
         function generateSquareElement(quantyOfSquares){
             const newSquare = document.createElement('div');
-            newSquare.classList.add('ms_square');
+            newSquare.classList.add(`ms_square_${quantyOfSquares}`  );
+
             newSquare.innerHTML = `<span>${i}</span>`;
             return newSquare;
         }
-        const newGenerateSquare = generateSquareElement(test);
+        const newGenerateSquare = generateSquareElement(difficoltas);
         newGenerateSquare.addEventListener('click', handleSquareClick);
         gridContainer.appendChild(newGenerateSquare);
     }
@@ -23,5 +36,8 @@ function startGame(){
 
 function handleSquareClick() {
     this.classList.add('active');
-    const thisSquareNumber =  this.querySelector('span').textContent ;
+    
 }
+
+
+
